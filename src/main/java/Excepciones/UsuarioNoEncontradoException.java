8d388ba4d4bdf7e864689cdaadcf492823409415
Villa <villahuertasmiguelangel11@gmail.com>
2026-08-21 18:45:19 -0500

    Cambio Sintaxix

--- src/main/java/Excepciones/UsuarioNoEncontradoException.java
+++ src/main/java/Excepciones/UsuarioNoEncontradoException.java
@@ -3,7 +3,6 @@ package Excepciones;
 public class UsuarioNoEncontradoException extends Exception {
 
     public UsuarioNoEncontradoException(int id) {
-
         super("No existe un usuario con el ID: " + id);
     }
 }
